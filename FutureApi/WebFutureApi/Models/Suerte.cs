144e61328ad52437788e18816597a69b5923cb76
MatiasAlemanA <a2019114089@estudiantes.upsa.edu.bo>
2021-10-13 21:13:29 -0400

    Model suerte creado

---
+++ FutureApi/WebFutureApi/Models/Suerte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFutureApi.Models
+{
+    public class Suerte
+    {
+        [Key]
+        public int FutureId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo es obligatorio")]
+        [StringLength(10, MinimumLength =1, ErrorMessage = "La longitud es erronea")]
+        public string Vision { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength =5, ErrorMessage ="Campo insuficiente")]
+        public string Imagen { get; set; }
+
+    }
+}
