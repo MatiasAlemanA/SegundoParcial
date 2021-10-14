c5d53cbd64722f774a4a4f57086f515dfb46317c
MatiasAlemanA <a2019114089@estudiantes.upsa.edu.bo>
2021-10-13 21:40:19 -0400

    Controler realizado

---
+++ FutureApi/WebFutureApi/Migrations/20211014013910_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebFutureApi.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suerte",
+                columns: table => new
+                {
+                    FutureId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Vision = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suerte", x => x.FutureId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suerte");
+        }
+    }
+}
